be07271733917ece506f8ff98f6ab46fdb1ddb78
astrarae <astrarae.stelr@outlook.com>
2025-10-07 23:31:11 +0500

    Added fix for image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             h="100vh"
             display="block"
             overflow="hidden"
-            bgImage="url('https://images.unsplash.com/photo-1510172951991-856a654063f9?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" 
+            bgImage="url('green-background.jpg')" 
             bgSize="cover"                  
             bgPosition="center"             
             bgRepeat="no-repeat"
